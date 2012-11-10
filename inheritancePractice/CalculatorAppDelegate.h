139b1f140228d0f54c17111fc16572b060bdbfaf
charliejimi <charliejimi@gmail.com>
2012-11-10 14:41:56 +0800

    Initial Commit

---
+++ inheritancePractice/CalculatorAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CalculatorAppDelegate.h
+//  inheritancePractice
+//
+//  Created by Charlie  on 12/11/10.
+//  Copyright (c) 2012年 Charlie . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ inheritancePractice/CalculatorViewController.m
@@ -0,0 +1,29 @@
+//
+//  CalculatorViewController.m
+//  inheritancePractice
+//
+//  Created by Charlie  on 12/11/10.
+//  Copyright (c) 2012年 Charlie . All rights reserved.
+//
+
+#import "CalculatorViewController.h"
+
+@interface CalculatorViewController ()
+
+@end
+
+@implementation CalculatorViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
